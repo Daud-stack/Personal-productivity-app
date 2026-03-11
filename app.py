9b3408a5417d208d4f4f59e5a32430906b606629
Daud Sibanda <58472949+Daud-stack@users.noreply.github.com>
2026-03-11 22:13:30 +0200

    Trigger deployment refresh

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template # Render build trigger
 from extensions import db, login_manager, csrf, migrate, mail
 from models import User
 
